9a7b92d853d25cc07939dad1ec0b1943921b1f68
gabrieldss <gabriel-santosouza@hotmail.com>
2020-03-12 22:44:24 -0300

    Ajustes com toBeDeepCloseTo

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -1,22 +1,30 @@
-const Calculadora = require ('../lib/calculadora')
+const { toBeDeepCloseTo }
+    = require ('jest-matcher-deep-close-to');
+    expect.extend({ toBeDeepCloseTo });
+
+const Calculadora = require ('../lib/calculadora');
+let calculadora = new Calculadora();
+
 
 describe('Calculadora', () => {
     test('cliente que ganha menos de 1000', () => {
-        let cliente = {
+        let props = calculadora.montarPropostas({
             nome: 'Joao da Silva',
             salario: 800.00,
             idade: 29,
             valorFinanciado: 1000.00
-        };
-        let calculadora = new Calculadora();
-        let props = calculadora.montarPropostas(cliente);
+        });
         expect(props.length).toBe(2);
-        expect(props[0].total).toBeCloseTo(2000.00);
-        expect(props[0].parcelas).toBe(2);
-        expect(props[0].valorParcela).toBeCloseTo(1000.00);
-        expect(props[1].total).toBeCloseTo(2000.00);
-        expect(props[1].parcelas).toBe(3);
-        expect(props[1].valorParcela).toBeCloseTo(666.666);
+        expect(props[0]).toBeDeepCloseTo({
+            total: 2000.00,
+            parcelas: 2,
+            valorParcela: 1000.00
+        },2);
+        expect(props[1]).toBeDeepCloseTo({
+            total: 2000.00,
+            parcelas: 3,
+            valorParcela: 666.66
+        },2);
     });
         test('cliente que ganha ate 5000', () => {
             let cliente2 = {
@@ -25,7 +33,6 @@ describe('Calculadora', () => {
                 idade: 29,
                 valorFinanciado: 1000.00
             };
-            let calculadora = new Calculadora();
             let props2 = calculadora.montarPropostas2(cliente2);
             expect(props2.length).toBe(3);
             expect(props2[0].total).toBeCloseTo(1300.00);
@@ -45,7 +52,6 @@ describe('Calculadora', () => {
                 idade: 29,
                 valorFinanciado: 1000.00
             };
-            let calculadora = new Calculadora();
             let props3 = calculadora.montarPropostas3(cliente3);
             expect(props3.length).toBe(4);
             expect(props3[0].total).toBeCloseTo(1100.00);
